2df52ee89ad51a169dc9996fcc4895cc5eda1407
Burhan <alpagut1993@gmail.com>
2024-03-03 14:05:26 +0100

    last name modified

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -61,7 +61,7 @@ export async function POST(req: Request) {
 
   if (eventType === "user.created") {
     console.log("route is worked");
-    console.log(evt.data);
+    console.log("heyyyy", evt.data);
     const { id, email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
@@ -70,7 +70,7 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
-      lastName: last_name,
+      lastName: `${first_name}${last_name ? ` ${last_name}` : ""}`,
       photo: image_url,
     };
 
