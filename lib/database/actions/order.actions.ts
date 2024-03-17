930ea9528da164b539d8d17d406b6c5757856a22
Burhan <alpagut1993@gmail.com>
2024-03-17 11:59:55 +0100

    sd

--- lib/database/actions/order.actions.ts
+++ lib/database/actions/order.actions.ts
@@ -22,6 +22,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
   const price = Number(order.price) * 100;
+  console.log("Price in cents:", price);
 
   try {
     const session = await stripe.checkout.sessions.create({
