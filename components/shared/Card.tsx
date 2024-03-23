0ebfcfc9b1d1ca005818809c3b4143319e172cb6
Burhan <alpagut1993@gmail.com>
2024-03-23 12:31:21 +0100

    bg-cover:discard

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -27,7 +27,7 @@ const Card = ({ event, hasOrderLink, hidePrice, homePage }: CardProps) => {
       <Link
         href={`/events/${event._id}`}
         style={{ backgroundImage: `url(${event.imageUrl})` }}
-        className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+        className="flex-center flex-grow bg-gray-50 bg-cover  text-grey-500"
       />
       {/* IS EVENT CREATOR ... */}
 
