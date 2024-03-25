759318ac98601c7a4882631bd051b7e5f017161f
Burhan <alpagut1993@gmail.com>
2024-03-25 14:52:22 +0100

    23

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -68,7 +68,7 @@ export async function POST(req: Request) {
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
-
+      lastName: last_name,
       photo: image_url,
     };
 
