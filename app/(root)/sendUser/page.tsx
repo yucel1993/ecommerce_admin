7af9f2815da0c6defb99f2ff89a20270cc36de5b
Burhan <alpagut1993@gmail.com>
2024-03-27 14:14:50 +0100

    signOut

--- app/(root)/sendUser/page.tsx
+++ app/(root)/sendUser/page.tsx
@@ -1,15 +1,19 @@
+import { SignOutButton } from "@clerk/nextjs";
+
 const UserSender = () => {
   return (
     <div className="flex justify-center items-center h-full max-w-[1000px] mx-auto ">
-      <a
-        href="https://ecommerce-admin-gray-seven.vercel.app"
-        rel=" noopener"
-        target="_blank"
-        className="text-purple-500 font-bold text-xl underline"
-      >
-        Click to sign in in the new tab 🚀 to carry out your crediantials
-        securely
-      </a>
+      <SignOutButton>
+        <a
+          href="https://ecommerce-admin-gray-seven.vercel.app"
+          rel=" noopener"
+          target="_blank"
+          className="text-purple-500 font-bold text-xl underline"
+        >
+          Click to sign in in the new tab 🚀 to carry out your crediantials
+          securely
+        </a>
+      </SignOutButton>
     </div>
   );
 };
