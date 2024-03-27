26b40c474d12501c53787c7397b5ff8cdfa97571
Burhan <alpagut1993@gmail.com>
2024-03-27 14:04:14 +0100

    usersednPage added

---
+++ app/(root)/sendUser/page.tsx
@@ -0,0 +1,17 @@
+const UserSender = () => {
+  return (
+    <div className="flex justify-center items-center h-full max-w-[1000px] mx-auto ">
+      <a
+        href="https://ecommerce-admin-gray-seven.vercel.app"
+        rel=" noopener"
+        target="_blank"
+        className="text-purple-500 font-bold text-xl underline"
+      >
+        Click to sign in in the new tab 🚀 to carry out your crediantials
+        securely
+      </a>
+    </div>
+  );
+};
+
+export default UserSender;
