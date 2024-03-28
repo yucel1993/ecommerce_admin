2be8b15a20cf99e5b022f8cd8fea0f4e90a32f79
Burhan <alpagut1993@gmail.com>
2024-03-28 14:47:58 +0100

    error

---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+const Error = () => {
+  return (
+    <div className="flex max-w-[1000px] mx-auto justify-center items-center">
+      <p className="text-bold text-3xl text-purple-500">
+        Opps Connection has been lost refresh the page again 🚀
+      </p>
+    </div>
+  );
+};
+
+export default Error;
