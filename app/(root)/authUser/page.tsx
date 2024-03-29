54c16e15e306d688b266a7cd89bce15c16c0edb4
Burhan <alpagut1993@gmail.com>
2024-03-29 11:56:47 +0100

    authUser

---
+++ app/(root)/authUser/page.tsx
@@ -0,0 +1,16 @@
+const AuthUser = () => {
+  return (
+    <div className="flex flex-col h-full max-w-[1100px] justify-center items-center mx-auto ">
+      <p className=" font-bold text-2xl text-purple-500">
+        Registration Successfull 🙂
+      </p>
+      <p className="font-bold text-2xl text-blue-500/100">
+        Either refresh the Page or close this session and open a new session to
+        validate your crediantials 🚀
+      </p>
+      <p className="font-bold text-2xl text-red-600">Happy Shoppings ❤</p>
+    </div>
+  );
+};
+
+export default AuthUser;
