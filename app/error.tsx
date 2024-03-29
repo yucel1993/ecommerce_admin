bfd338ca2b68bf37ebd6b7fe9288d815a10328af
Burhan <alpagut1993@gmail.com>
2024-03-29 13:59:50 +0100

    error.tsx

---
+++ app/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="flex flex-col h-full gap-y-10 max-w-[1100px] justify-center items-center mx-auto ">
+      <h2 className=" font-bold text-2xl text-purple-500">
+        Ops, Something went wrong!
+      </h2>
+      <button onClick={() => reset()}>Try again</button>
+      <p className="font-bold text-2xl text-blue-500/100 text-center">
+        Or close this session and open a new session 🚀
+      </p>
+    </div>
+  );
+}
